fd7ca49391976eb697f74205d72936bf985bc7d7
Siddu1996 <30632808+Siddu1996@users.noreply.github.com>
2020-04-27 14:11:07 +0530

    Update mvn.sh

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,8 +4,8 @@ echo "*************************************"
 echo "** TESTING THE CODE *********************"
 echo "*************************************"
 
-# WORKSPACE=/var/lib/jenkins/workspace/shop-maven-pipeline
+WORKSPACE=/var/lib/jenkins/workspace/stock-maven-app
 
 
-docker run --rm -v $PWD/stockmanager:/stockmanager-s -v /root/.m2/:/root/.m2/ -w /stockmanager-s maven:latest "$@"
+docker run --rm -v $WORKSPACE/stockmanager:/stockmanager-s -v /root/.m2/:/root/.m2/ -w /stockmanager-s maven:latest "$@"
 
