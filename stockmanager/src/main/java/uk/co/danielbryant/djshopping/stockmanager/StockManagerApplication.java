1a21239072b79ee75fb6f37138dea5b542682516
Siddu1996 <30632808+Siddu1996@users.noreply.github.com>
2020-05-20 13:48:30 +0530

    Original change

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplication.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplication.java
@@ -8,6 +8,6 @@ public class StockManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StockManagerApplication.class, args);
-        System.out.println("hello this is git-hub");
+        System.out.println("hello this is github");
     }
 }
